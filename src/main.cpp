1c26a196be6e062e0c720031bd70ef398f126837
48exa <97523839+48exa@users.noreply.github.com>
2024-02-12 19:19:56 +0100

    Starting over

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,16 @@
 #include "raylib.h"
-#include "scene.h"
-#include "../framework/timer.h"
 
 int main()
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
-
-    // Scene *scene = new Scene();
-    // Timer *timer = new Timer();
+    InitWindow(1280, 720, "Window title");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
-        // scene->update(GetFrameTime());
-        // timer->update(GetFrameTime());
-
-        // if (timer->time() == 1)
-        // {
-        //     std::cout << "FPS:\t" << GetFPS() << "fps\tTime per frame:\t" << GetFPS() / 1000 << "ms" << std::endl;
-        //     timer->reset();
-        // }
-
-        DrawFPS(100, 100);
+        ClearBackground(BLACK);
+        DrawText("Hello World!", 10, 30, 16, WHITE);
+        DrawFPS(10, 10);
 
         EndDrawing();
     }
